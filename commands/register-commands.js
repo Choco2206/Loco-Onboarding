ba9a72bb5ddcd549fc92006456811868a05b3437
Choco2206 <k.alvaro@gmx.de>
2026-03-26 14:00:17 +0100

    register-commands.js aktualisieren

--- commands/register-commands.js
+++ commands/register-commands.js
@@ -1,1 +1,27 @@
-create
+require('dotenv').config();
+
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('onboarding-panel')
+    .setDescription('Öffnet das Admin Panel für das Loco Onboarding.')
+    .toJSON(),
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Slash Commands werden registriert...');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+
+    console.log('Slash Commands erfolgreich registriert.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
